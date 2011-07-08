c71740fea6694108812d51abf5c2567737f11841
Vladislav Shinkarenko <vadic7wee2@gmail.com>
2011-07-08 22:55:33 +0300

    added sample components

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,14 +2,40 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
-public class Main extends Activity
+
+import java.awt.*;
+import java.util.Date;
+
+public class Main extends Activity implements CompoundButton.OnCheckedChangeListener
 {
+    Button btn;
+    CheckBox chb;
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate(Bundle icicle)
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate(icicle);
         setContentView(R.layout.main);
+        btn = new Button(this);
+        chb = (CheckBox)findViewById(R.id.checkbox);
+        chb.setOnCheckedChangeListener(this);
+
+
+        EditText field = (EditText)findViewById(R.id.field);
+        field.setText("This text was set from the main activity" +
+                "\"Licence\" used of apache");
+    }
+
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if(isChecked)
+            chb.setText("This checkbox is:checked");
+        else
+            chb.setText("This checkbox is:unchecked");
     }
 }
